2c04584dbc6d1780097e52bdc6cedd1c3ef9fab3
Manuel Küblböck <manuel@qualityswdev.com>
2015-10-02 16:26:20 +0200

    Add simple search.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+document.addEventListener("DOMContentLoaded", function(event) {
+  SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: '/search.json',
+    searchResultTemplate: '<li><a href="{url}">{title}</a></li>'
+  })
+});

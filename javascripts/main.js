88a331f0427696ed97c1730a382e5d2077fdc2a0
Dan Ackerson <dan@ackerson.de>
2018-06-08 10:39:14 +0200

    fix search

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   SimpleJekyllSearch({
     searchInput: document.getElementById('search-input'),
     resultsContainer: document.getElementById('results-container'),
-    json: 'search.json',
+    json: 'docs/search.json',
     searchResultTemplate: '<li><a href="{url}">{title}</a></li>'
   })
 });
